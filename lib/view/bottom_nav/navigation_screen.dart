b3615f560bb060ebde978d1469aa393d8506e166
AthiraaAthi <athiraa2612@gmail.com>
2023-11-10 07:47:08 +0530

    gave design to watchScreen Tab

--- lib/view/bottom_nav/navigation_screen.dart
+++ lib/view/bottom_nav/navigation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pinterest_ui/view/add_screen/add_screen.dart';
+import 'package:pinterest_ui/utils/color_constant/color_constant.dart';
+
 import 'package:pinterest_ui/view/home_screen/browse_screen.dart';
 import 'package:pinterest_ui/view/home_screen/home_screen.dart';
 import 'package:pinterest_ui/view/home_screen/watch_screen.dart';
@@ -20,7 +21,7 @@ class _NavigationScreenState extends State<NavigationScreen> {
   List<Widget>MyWidgetList=[
    HomeScreen(),
     SearchScreen(),
-    AddScreen(),
+   
     NotificationScreen(),
     ProfileScreen()
     
@@ -48,7 +49,41 @@ class _NavigationScreenState extends State<NavigationScreen> {
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.search),label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.add),label: ""),
+          BottomNavigationBarItem(icon: InkWell( //for navigating directly
+            onTap: (){
+              showModalBottomSheet(
+                backgroundColor: ColorConstant.BarBlack,
+                context: context, builder: (context) => Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(40)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   
+                   
+                    Center(
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(color: Color.fromARGB(255, 65, 61, 61),borderRadius: BorderRadius.circular(20),),
+                        child: Icon(Icons.push_pin,color: Colors.white,),
+                        ),
+                    ),
+                     
+                      SizedBox(width: 20,),
+                       Center(
+                         child: Container(
+                                             height: 70,
+                                             width: 70,
+                                             decoration: BoxDecoration(color: Color.fromARGB(255, 65, 61, 61),borderRadius: BorderRadius.circular(20)),
+                                             child: Icon(Icons.space_dashboard,color: Colors.white,),
+                                             ),
+                       ),
+                      
+                  ],
+                ),
+              ));
+            },
+            child: Icon(Icons.add)),label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.message_rounded),label: ""),
           BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: ""),
 
--- lib/view/home_screen/watch_screen.dart
+++ lib/view/home_screen/watch_screen.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:pinterest_ui/main.dart';
+import 'package:pinterest_ui/utils/image_constant/image_constant.dart';
 
 
 
 
 class WatchScreen extends StatelessWidget {
-  const WatchScreen({super.key});
+  WatchScreen({super.key});
+  List<String>MyList=[
+     "assets/images/pexels-andrei-tanase-1271620.jpg",
+    "assets/images/pexels-delcho-dichev-3659194.jpg",
+    "assets/images/pexels-dom-le-roy-3567673.jpg",
+    "assets/images/pexels-efrain-alonso-3584283.jpg",
+    "assets/images/pexels-ivica-džambo-3273786.jpg",
+    "assets/images/pexels-jonathan-borba-2878710.jpg",
+    "assets/images/pexels-kehn-hermano-3881036.jpg",
+    "assets/images/pexels-leah-kelley-618902.jpg",
+    "assets/images/pexels-nothing-ahead-13888309.jpg",
+    "assets/images/pexels-oliver-sjöström-1047051.jpg",
+    "assets/images/pexels-prashant-gautam-3783385.jpg",
+    "assets/images/pexels-roman-odintsov-6898857.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        controller: PageController(),
+        itemCount:MyList.length ,
+        itemBuilder: (context, index) => Container(
+        
+        decoration: BoxDecoration(color: Colors.blue,image: DecorationImage(image: AssetImage(MyList[index]),fit: BoxFit.cover)),
+      ),),
       
     );
    
